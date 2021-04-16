73dd97da0c03b25662dec826fdbf6f5ca4914ce0
pattons-OSU <pattons@oregonstate.edu>
2021-04-15 20:03:07 -0700

    Creating and adding intorduction to password progr

--- password.py
+++ password.py
@@ -1,1 +1,12 @@
-##Simeon Patton
+#! /usr/bin/env python3
+
+"""
+    Simeon Patton
+    OSU CS362 Spring 2021
+    In class activity 2 - Git in action
+
+        b) Create a program to find all of the nubers that a number is evenly divisible by.
+
+            Some code inspiration from GeeksforGeeks @
+            https://www.geeksforgeeks.org/find-divisors-natural-number-set-1/
+"""
