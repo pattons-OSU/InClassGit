15e7d4f89fb94697ce70a6b1b19b5af9867b839c
pattons-OSU <pattons@oregonstate.edu>
2021-04-15 19:24:52 -0700

    Cleaning up UI

--- calc.py
+++ calc.py
@@ -4,7 +4,8 @@
 
 def calc(a, b):
     sum = a + b
-    print(sum)
+    f"The sum of {a} + {b} is {sum}."
+    ##print(sum)
 
     difference = a - b
     print(difference)
@@ -21,13 +22,20 @@ def calc(a, b):
     SumOfList = FinalValues[0] + FinalValues[1] + FinalValues[2] + FinalValues[3]
     print(SumOfList)
 
-a = input("Please enter a number 'a': ")
-a = int(a)
-print("")
+def a_input():
+    a = input("Please enter a number 'a': ")
+    a = int(a)
+    print("")
+    return int(a)
 
-b = input("Please enter a number 'b': ")
-print("")
-b = int(b)
+def b_input():
+    b = input("Please enter a number 'b': ")
+    print("")
+    b = int(b)
+    return int(b)
 
+
+a = a_input()
+b = b_input()
 calc(a,b)
 print("")
