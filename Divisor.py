28db481f964eb85888cd25f31549c8d197bc06dd
pattons-OSU <pattons@oregonstate.edu>
2021-04-15 19:47:07 -0700

    Setting up description and divisor program

--- Divisor.py
+++ Divisor.py
@@ -1,1 +1,25 @@
-## Simeon Patton
+#! /usr/bin/env python3
+
+"""
+    Simeon Patton
+    OSU CS362 Spring 2021
+    In class activity 2 - Git in action
+
+        b) Create a program to find all of the nubers that a number is evenly divisible by.
+"""
+
+def divisor(a):
+
+    
+
+def a_input():
+    a = input("Please enter a number to find it's divisors: ")
+    a = int(a)
+    print("")
+    return int(a)
+
+if __name__ == "__main__":
+    print("")
+    a = a_input()
+    ##divisor(a)
+    print("")
