4c8c61d8e819dcc9d8ad149a6f3076f70cf5a463
pattons-OSU <pattons@oregonstate.edu>
2021-04-15 19:59:41 -0700

    fiishing divisor function

--- Divisor.py
+++ Divisor.py
@@ -6,9 +6,20 @@
     In class activity 2 - Git in action
 
         b) Create a program to find all of the nubers that a number is evenly divisible by.
+
+            Some code inspiration from GeeksforGeeks @ 
+            https://www.geeksforgeeks.org/find-divisors-natural-number-set-1/
 """
 
 def divisor(a):
+    ## Number to enumerate by
+    i = 1
+    print(f"The even divisors of {a} are: ")
+    ## If given number is even mod divisible then print value and move on
+    while i <= a:
+        if (a % i == 0):
+            print(i)
+        i = i + 1
 
     
 
@@ -20,6 +31,6 @@ def a_input():
 
 if __name__ == "__main__":
     print("")
-    ##a = a_input()
+    a = a_input()
     divisor(a)
     print("")
