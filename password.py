ed1a04656072d6f410541c9c61698cac701e4655
pattons-OSU <pattons@oregonstate.edu>
2021-04-15 20:06:21 -0700

    updating intoduction

--- password.py
+++ password.py
@@ -5,8 +5,5 @@
     OSU CS362 Spring 2021
     In class activity 2 - Git in action
 
-        b) Create a program to find all of the nubers that a number is evenly divisible by.
-
-            Some code inspiration from GeeksforGeeks @
-            https://www.geeksforgeeks.org/find-divisors-natural-number-set-1/
+        c) Create a password Generator by use of random characters given a number from the user.
 """
